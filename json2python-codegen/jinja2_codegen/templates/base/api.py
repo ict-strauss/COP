16e8aa459f839a5d03efd0312c41cb5e45075516
lukasbIP <lukas.baumbauer@tum.de>
2015-08-07 18:36:49 +0200

    Remove some demo code from the base template.

--- json2python-codegen/jinja2_codegen/templates/base/api.py
+++ json2python-codegen/jinja2_codegen/templates/base/api.py
@@ -25,93 +25,6 @@ urls = (
 users = {{users}}
 {% endif %}
 
-def byteify(input):
-    # Convert JSON unicode strings to python byte strings, recursively on a json_struct
-    if isinstance(input, dict):
-        return {byteify(key):byteify(value) for key,value in input.iteritems()}
-    elif isinstance(input, list):
-        return [byteify(element) for element in input]
-    elif isinstance(input, unicode):
-        return input.encode('utf-8')
-    else:
-        return input
-
-def json_loads(json_string):
-    # Try to use json.loads and raise HTTP Error
-    try:
-        json_struct = json.loads(json_string) #json parser.
-    except ValueError:
-        raise BadRequestError("Malformed JSON")
-    else:
-        return byteify(json_struct)
-
-def json_dumps(js):
-    # Pretty-print version of json.dumps
-    return json.dumps(js, sort_keys=True, indent=4, separators=(',', ': '))
-
-def create_instance(klass, json_struct, id=None):
-    # Try to create an object instance and raise HTTP Errors
-    try:
-        new_object = klass(json_struct) # Creates an object instance of type klass from the json_struct data
-    except KeyError as inst:
-        raise BadRequestError("Unknown entity name in JSON:" + "<br>" + inst.args[0])
-    except TypeError as inst:
-        key = inst.args[0]
-        value = json.dumps(inst.args[1])
-        raise BadRequestError("Incorrect type in JSON:" + "<br>" +
-                              key + " was:" + "<br>" +
-                              value + "<br>" +
-                              "Allowed type:" + "<br>" +
-                              inst.args[2])
-    except ValueError as inst:
-        if type(inst.args[1]) == str:
-            raise BadRequestError("Incorrect value in JSON:" + "<br>" +
-                                  "Enum " + inst.args[0] + " was:" + "<br>" +
-                                  inst.args[1] + "<br>" +
-                                  "Allowed values:" + "<br>" +
-                                  "[" + ", ".join(inst.args[2]) + "]")
-        elif type(inst.args[1]) == int:
-            raise BadRequestError("Incorrect value in JSON:" + "<br>" +
-                                  "Enum " + inst.args[0] + " was:" + "<br>" +
-                                  str(inst.args[1]) + "<br>" +
-                                  "Allowed range:" + "<br>" +
-                                  "1 - " + str(inst.args[2]))
-    else:
-        # Check if the id given in the URL matches the id given in the body
-        if id != None and id[0] != getattr(new_object, id[1]):
-            raise BadRequestError(id[1] + " in body not matching " + id[1] + " in URL")
-        else:
-            return new_object
-
-def modify_instance(existing_object, json_struct):
-    try:
-        existing_object.load_json(json_struct)
-    except KeyError as inst:
-        raise BadRequestError("Unknown entity name in JSON:" + "<br>" + inst.args[0])
-    except TypeError as inst:
-        key = inst.args[0]
-        value = json.dumps(inst.args[1])
-        raise BadRequestError("Incorrect type in JSON:" + "<br>" +
-                              key + " was:" + "<br>" +
-                              value + "<br>" +
-                              "Allowed type:" + "<br>" +
-                              inst.args[2])
-    except ValueError as inst:
-        if type(inst.args[1]) == str:
-            raise BadRequestError("Incorrect value in JSON:" + "<br>" +
-                                  "Enum " + inst.args[0] + " was:" + "<br>" +
-                                  inst.args[1] + "<br>" +
-                                  "Allowed values:" + "<br>" +
-                                  "[" + ", ".join(inst.args[2]) + "]")
-        elif type(inst.args[1]) == int:
-            raise BadRequestError("Incorrect value in JSON:" + "<br>" +
-                                  "Enum " + inst.args[0] + " was:" + "<br>" +
-                                  str(inst.args[1]) + "<br>" +
-                                  "Allowed range:" + "<br>" +
-                                  "1 - " + str(inst.args[2]))
-    else:
-        return existing_object
-
 class NotFoundError(web.HTTPError):
     def __init__(self,message):
         status = '404 '+message
@@ -166,23 +79,6 @@ class {{callback.name}}:
         {% if cors %}
         web.header('Access-Control-Allow-Origin','{{url}}')
         {% endif %}
-        try:
-            existing_object = {{callback.name}}Impl.get({{callback.arguments|join(', ')}})
-        except KeyError as inst:
-            if inst.args[0] != '{{callback.arguments|last()}}':
-                raise NotFoundError(inst.args[0] + " not found")
-            json_string = web.data()
-            json_struct = json_loads(json_string)
-        {% if callback.check_id %}
-            new_object = create_instance({{callback.thing}}, json_struct, ({{callback.arguments|last()}},'{{callback.arguments|last()}}'))
-        {% else %}
-            new_object=create_instance({{callback.thing}}, json_struct)
-        {% endif %}
-            {{callback.name}}Impl.put({{callback.arguments|join(', ')}}, new_object)
-            js=new_object.serialize_json()
-            raise Successful("Successful operation",json_dumps(js))
-        else:
-            raise BadRequestError("Object already exists. For updates use POST.")
     {% endif %}
     {% if callback.methods['POST'] %}
 
@@ -197,26 +93,6 @@ class {{callback.name}}:
         {% if cors %}
         web.header('Access-Control-Allow-Origin','{{url}}')
         {% endif %}
-        json_string=web.data()
-        json_struct=json_loads(json_string)
-        try:
-            existing_object = {{callback.name}}Impl.get({{callback.arguments|join(', ')}})
-        except KeyError as inst:
-            if inst.args[0] != '{{callback.arguments|last()}}':
-                raise NotFoundError(inst.args[0] + " not found")
-        {% if callback.check_id %}
-            new_object=create_instance({{callback.thing}}, json_struct, ({{callback.arguments|last()}},'{{callback.arguments|last()}}'))
-        {% else %}
-            new_object=create_instance({{callback.thing}}, json_struct)
-        {% endif %}
-            {{callback.name}}Impl.post({{callback.arguments|join(', ')}}, new_object)
-            js=new_object.serialize_json()
-            raise Successful("Successful operation",json_dumps(js))
-        else:
-            existing_object = modify_instance(existing_object, json_struct)
-            {{callback.name}}Impl.post({{callback.arguments|join(', ')}}, existing_object)
-            js=existing_object.serialize_json()
-            raise Successful("Successful operation",json_dumps(js))
     {% endif %}
     {% if callback.methods['DELETE'] %}
 
@@ -231,12 +107,6 @@ class {{callback.name}}:
         {% if cors %}
         web.header('Access-Control-Allow-Origin','{{url}}')
         {% endif %}
-        try:
-            response={{callback.name}}Impl.delete({{callback.arguments|join(', ')}})
-        except KeyError as inst:
-            raise NotFoundError(inst.args[0] + " not found")
-        else:
-            raise Successful('Successful operation')
     {% endif %}
     {% if callback.methods['GET'] %}
 
@@ -251,13 +121,6 @@ class {{callback.name}}:
         {% if cors %}
         web.header('Access-Control-Allow-Origin','{{url}}')
         {% endif %}
-        try:
-            response = {{callback.name}}Impl.get({{callback.arguments|join(', ')}})
-        except KeyError as inst:
-            raise NotFoundError(inst.args[0] + " not found")
-        else:
-            js = response.serialize_json()
-            raise Successful("Successful operation",json_dumps(js))
     {% endif %}
     {% if cors %}
 
--- json2python-codegen/jinja2_codegen/templates/base/callable.py
+++ json2python-codegen/jinja2_codegen/templates/base/callable.py
@@ -1,6 +1,5 @@
 import os.path, sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__))))
-from backend import {{toplevel}}
 
 class {{class_name}}Impl:
     {% if methods['PUT'] %}
@@ -11,7 +10,6 @@ class {{class_name}}Impl:
         print str({{methods['PUT'].printstr}})
         {% endif %}
         print 'handling put'
-        cls.post({{methods['PUT'].arguments|join(', ')}})
     {% endif %}
     {% if methods['POST'] %}
 
@@ -21,21 +19,6 @@ class {{class_name}}Impl:
         print str({{methods['POST'].printstr}})
         {% endif %}
         print 'handling post'
-        {% if methods['POST'].arguments | length > 2 %}
-        if {{methods['POST'].arguments[0]}} in {{object_path[0]}}:
-            {% if methods['POST'].arguments | length > 3 %}
-            if {{methods['POST'].arguments[1]}} in {{object_path[1]}}:
-                {{object_path[2]}}[{{methods['POST'].arguments[2]}}]{{ending}} = {{methods['POST'].arguments[3]}}
-            else:
-                raise KeyError('{{methods['POST'].arguments[1]}}')
-            {% else %}
-            {{object_path[1]}}[{{methods['POST'].arguments[1]}}]{{ending}} = {{methods['POST'].arguments[2]}}
-            {% endif %}
-        else:
-            raise KeyError('{{methods['POST'].arguments[0]}}')
-        {% else %}
-        {{object_path[0]}}[{{methods['POST'].arguments[0]}}]{{ending}} = {{methods['POST'].arguments[1]}}
-        {% endif %}
     {% endif %}
     {% if methods['DELETE'] %}
 
@@ -45,24 +28,6 @@ class {{class_name}}Impl:
         print str({{methods['DELETE'].printstr}})
         {% endif %}
         print 'handling delete'
-        if {{methods['DELETE'].arguments[0]}} in {{object_path[0]}}:
-            {% if methods['DELETE'].arguments | length > 1 %}
-            if {{methods['DELETE'].arguments[1]}} in {{object_path[1]}}:
-                {% if methods['DELETE'].arguments | length > 2 %}
-                if {{methods['DELETE'].arguments[2]}} in {{object_path[2]}}:
-                    del({{object_path[2]}}[{{methods['DELETE'].arguments[2]}}]{{ending}})
-                else:
-                    raise KeyError('{{methods['DELETE'].arguments[2]}}')
-                {% else %}
-                del({{object_path[1]}}[{{methods['DELETE'].arguments[1]}}]{{ending}})
-                {% endif %}
-            else:
-                raise KeyError('{{methods['DELETE'].arguments[1]}}')
-            {% else %}
-            del({{object_path[0]}}[{{methods['DELETE'].arguments[0]}}]{{ending}})
-            {% endif %}
-        else:
-            raise KeyError('{{methods['DELETE'].arguments[0]}}')
     {% endif %}
     {% if methods['GET'] %}
 
@@ -72,27 +37,4 @@ class {{class_name}}Impl:
         print str({{methods['GET'].printstr}})
         {% endif %}
         print 'handling get'
-        {% if methods['GET'].arguments | length > 0 %}
-        if {{methods['GET'].arguments[0]}} in {{object_path[0]}}:
-            {% if methods['GET'].arguments | length > 1 %}
-            if {{methods['GET'].arguments[1]}} in {{object_path[1]}}:
-                {% if methods['GET'].arguments | length > 2 %}
-                if {{methods['GET'].arguments[2]}} in {{object_path[2]}}:
-                    return {{object_path[2]}}[{{methods['GET'].arguments[2]}}]{{ending}}
-                else:
-                    raise KeyError('{{methods['GET'].arguments[2]}}')
-                {% else %}
-                return {{object_path[1]}}[{{methods['GET'].arguments[1]}}]{{ending}}
-                {% endif %}
-            else:
-                raise KeyError('{{methods['GET'].arguments[1]}}')
-            {% else %}
-            return {{object_path[0]}}[{{methods['GET'].arguments[0]}}]{{ending}}
-            {% endif %}
-        else:
-            raise KeyError('{{methods['GET'].arguments[0]}}')
-        {% else %}
-        return {{object_path[0]}}
-        {% endif %}
-        
     {% endif %}
