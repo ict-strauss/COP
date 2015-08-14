dfa8e68194658a53c5db43188a57fe2d2952780d
lukasbIP <lukas.baumbauer@tum.de>
2015-08-14 16:27:25 +0200

    Add docs to jinja_classes.py.

--- json2python-codegen/jinja2_codegen/jinja_classes.py
+++ json2python-codegen/jinja2_codegen/jinja_classes.py
@@ -1,19 +1,27 @@
-class ImportObject(object):
+# These classes serve as a storage for variables passed to the jinja code generator in the template.render() method.
+# Instead of classes, dictionaries could be used as well, but classes document better which variables exist.
 
+class ImportObject(object):
+    # This is used together with the "import.py" template to generate the following python statement:
+    # from file import name
     def __init__(self, file, name):
         self.file = file
         self.name = name
 
 
 class AttributeObject(object):
-
+    # Used together with the "object.py" template, to generate attributes of a class, for example:
+    # self.name = value
     def __init__(self, name, value):
         self.name = name
         self.value = value
 
 
 class EnumObject(object):
-
+    # Use in the "object.py" template, to generate the defintion of class that inherits from EnumType:
+    # class Operstatus(EnumType):
+    #     possible_values = ['down', 'up']
+    #     range_end = 2
     def __init__(self, name, values):
         self.name = name
         self.values = values
@@ -21,14 +29,17 @@ class EnumObject(object):
 
 
 class UrlObject(object):
-
+    # Used in the template "api.py", to generate a web.py url to class mapping:
+    # urls = (
+    # "/restconf/config/connections/connection/(\w+)/match/" , "service_call.ConnectionsConnectionConnectionidMatch"
+    # )
     def __init__(self, path, callback):
         self.path = path
         self.callback = callback
 
 
 class CallbackObject(object):
-
+    # Used in the template "api.py", to generate the POST, PUT etc. methods.
     def __init__(self, name, path, methods, arguments, thing, check_id):
         self.name = name
         self.path = path
