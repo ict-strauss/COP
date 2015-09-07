ebe4734518f3d8b9ef34aa805f7738270e801f7c
Arturo Mayoral <arturo.mayoral@cttc.es>
2015-09-07 15:33:10 +0200

    Fix bug on include the path of the file

--- json2python-codegen/jinja2_codegen/templates/base/callable.py
+++ json2python-codegen/jinja2_codegen/templates/base/callable.py
@@ -1,5 +1,5 @@
 import os.path, sys
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(os.path.join('/'.join(os.path.dirname(os.path.realpath(__file__)).split('/')[:-1])))
 
 class {{class_name}}Impl:
     {% if methods['PUT'] %}
